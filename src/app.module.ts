6d6c01b676844f8e069d99efa1df099f1ce62159
Mokocup <watashi.wa.yui.desu@gmail.com>
2025-05-15 02:38:27 +0700

    feat: Add Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SourceConnectionsModule } from './source-connections/source-connections.module';
+import { SourceConnectionsModule } from './modules/source-connections/source-connections.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [SourceConnectionsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    // TODO: Convert this to Service and sync with typeORM CLI later when have time , fixed Config for now
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      // @ts-ignore
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      migrations: [__dirname + '/database/migrations/**/*{.ts,.js}'],
+    }),
+    SourceConnectionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Schema Importer API')
+    .setDescription(
+      'API for managing database source connections and retrieving schema information',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); //  Endpoint for Swagger UI
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
